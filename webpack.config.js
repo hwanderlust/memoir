f005c4201eae02020ba09a8f573d163c8d51562a
Paul <lpault29@gmail.com>
2018-12-02 16:13:57 -0500

    webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].[hash].js',
+    // filename: '[name].js',
+    // path: path.resolve(__dirname, '/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { 
+            loader: 'babel-loader',
+            options: {
+              plugins: ['transform-class-properties']
+            } 
+          },
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          { loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { minimize: true } },
+          { loader: 'postcss-loader', options: {
+            ident: 'postcss',
+            plugins: [require('autoprefixer')()]
+          } },
+          { loader: 'sass-loader' },
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\\/]node_modules[\\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+}
